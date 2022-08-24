663a13a5f87bbef017a1e732629f479d9b98cb9f
Iporã Possantti <61351163+ipo-exe@users.noreply.github.com>
2022-08-24 12:42:32 -0300

    Update geo.py

--- geo.py
+++ geo.py
@@ -1,6 +1,5 @@
 '''
-plans - planning nature-based solutions
-Version: 3.0
+PLANS - Planning Nature-based Solutions
 
 Module description:
 This is the PLANS 3 geoprocessing raster module.
@@ -8,7 +7,9 @@ This is the PLANS 3 geoprocessing raster module.
 Copyright (C) 2022 Iporã Brito Possantti
 
 ************ GNU GENERAL PUBLIC LICENSE ************
+
 https://www.gnu.org/licenses/gpl-3.0.en.html
+
 Permissions:
  - Commercial use
  - Distribution
@@ -23,16 +24,21 @@ Conditions:
 Limitations:
  - Liability
  - Warranty
+
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
+
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
+plans - planning nature-based solutions
+Version: 3.0
 
 '''
 import numpy as np
